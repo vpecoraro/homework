d94fb9b7cb97093bf3d497ba9ba5c346872d70e8
“vpecoraro” <victoria.cabrera1766@gmail.com>
2023-02-12 20:56:09 -0500

    practing

---
+++ Practice.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("i can drive");
+
+
+function logger() {
+    console.log('My name is jonas');
+}
+//calling /running/invoking function
+logger();
+logger();
+logger();
+logger();
