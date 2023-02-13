b144146aa607b45f3b366f4e7e426f8dc253403d
“vpecoraro” <victoria.cabrera1766@gmail.com>
2023-02-12 21:12:00 -0500

    finished lesson 7 activity 2

--- Lesson 07/02_data-types-practice/starter_code/script.js
+++ Lesson 07/02_data-types-practice/starter_code/script.js
@@ -5,14 +5,21 @@ const message = "hello";
 const recepient = "world";
 
 console.log(message + " " + recepient);
+console.log(`${message} ${recepient}`);
 
 // One plus one is...? Research as many ways as you can for adding 1 to the number variable.
 // Then, console.log the phrase "1 + 1 is 2" by using the updated value of the number variable.
 let number = 1;
-
+number = number + 1;
+console.log(number);
 // Data types can cause unexpected issues in Javascript. Use the typeof operator to investigate and log the type of the following variables. Your console messages should be formatted like so: "clearancePrice is a number"
 // Documentation on typeof: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
 const programmingIsCool = true;
 const clearancePrice = 1.99;
 const lifeTheUniverseAndEverything = 42;
 const theCruelestMonth = "April";
+
+console.log("programmingIsCool is a " + typeof programmingIsCool);
+console.log("clearancePrice is a " + typeof clearancePrice);
+console.log("lifeTheUniverseAndEverything is a", typeof lifeTheUniverseAndEverything);
+console.log("theCruelestMonth is a", typeof theCruelestMonth);
