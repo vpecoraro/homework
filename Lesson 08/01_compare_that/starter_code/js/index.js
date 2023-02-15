ccf0b054598d4698f9c454a8f8de6b083b654454
“vpecoraro” <victoria.cabrera1766@gmail.com>
2023-02-15 08:23:17 -0500

    Adding lesson 9

--- Lesson 08/01_compare_that/starter_code/js/index.js
+++ Lesson 08/01_compare_that/starter_code/js/index.js
@@ -1,12 +1,35 @@
 
- document.querySelector('#submit').onclick = function(e) {
-    e.preventDefault();
-     let comparison;
-     let a = Number(document.querySelector('#a').value);
-     let b = Number(document.querySelector('#b').value);
- 
-     // Add comparison logic
- 
-     document.querySelector('#comparison').innerText = comparison;
-   };
- 
+document.querySelector('#submit').onclick = function (e) {
+  e.preventDefault();
+  let comparison;
+  let a = Number(document.querySelector('#a').value);
+  let b = Number(document.querySelector('#b').value);
+
+  // Add comparison logic 
+  //our task is to figure out which value is greater and to put the correct value in the comparsion variable
+
+  //if A is greater than B 
+  if (a > b) {
+    comparison = ">";
+  }
+
+  //if B is greater than A
+  if (b > a) {
+    comparison = "<";
+  }
+
+  //if a and b are equal then show = 
+  if (a == b) {
+    comparison = "=";
+  }
+
+
+
+  //if a is not a number OR b is not a number then show N/A
+  if (isNaN(a) || isNaN(b)) {
+    comparison = "N/A";
+  }
+
+  document.querySelector('#comparison').innerText = comparison;
+};
+
---
+++ Lesson 09/02_rock_paper_scissors/starter_code/game.js
@@ -0,0 +1,49 @@
+
+
+
+
+// Create a function that determines the computer choice.  It should return one of the three values: 'rock', 'paper', or 'scissors'
+function computerChoice() {
+    //make a random number between 1 and 3
+
+    //this says give me a random number, multiple by 3 and round up 
+    let weNeedAName = Math.ceil(Math.random() * 3);
+
+    //if a number is 1, return rock
+
+    if (weNeedAName == 1) {
+        return "rock";
+    } else if (weNeedAName == 2) {
+        //if a number is 2, return scissors
+        return "scissors";
+    } else {
+        //else return paper
+        return "paper";
+    };
+
+
+    /* Create a function that compares the user's selection to the computer's selection.  It should then output the opponent's choice to #computerSelection and display who is the winner.
+    
+     */
+    function compare(me, opponent) {
+        let winner = ""length
+
+        //decide who wins
+        // if me==opponent
+
+        //if i am paper and opp is rock, 
+        //if I 
+
+    };
+
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+
+        // Bind a click event onto each of the three buttons.  It should run the compare function and then put the result to the #decsision id in the html
+    });
+
+
+
+
+
